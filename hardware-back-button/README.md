7976cd4d16f4b18c4e5911d14ff3098931e3f06e
Alberto Ventura <albertoventura.dev@gmail.com>
2022-09-17 00:43:42 -0300

    management stack works

--- hardware-back-button/README.md
+++ hardware-back-button/README.md
@@ -1,2 +1,15 @@
 ### This is a study project to show the way to work with android hardware back button in ionic/capacitor.
 
+### commands
+$ npm install --save @capacitor/core @capacitor/cli
+$ npx cap init
+$ ionic build
+$ npm install @capacitor/android
+$ npx cap add android
+$ npm install @capacitor/app                                             
+
+$ npx cap copy
+$ npx cap sync
+$ npx cap open android
+
+$ ionic build && npx cap sync && npx cap open android
--- hardware-back-button/src/app/app.component.ts
+++ hardware-back-button/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { AlertController, Platform } from '@ionic/angular';
-
+import { Component, ViewChild} from '@angular/core';
+import { AlertController, Platform, IonRouterOutlet } from '@ionic/angular';
+import { Location } from '@angular/common';
+/* import { Plugins } from '@capacitor/core';
+const { app } = Plugins; */
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild(IonRouterOutlet, {static: true}) routerOutlet: IonRouterOutlet;
   constructor(
     private platform: Platform,
     private alertcontroller: AlertController,
+    private location: Location,
   ) {
     this.initializeApp();
     this.backButtonEvent();
@@ -21,12 +25,27 @@ export class AppComponent {
   }
   backButtonEvent() {
     this.platform.backButton.subscribeWithPriority(10, ()=> {
-      this.backButtonAlert();
+      if(!this.routerOutlet.canGoBack()){
+        this.backButtonAlert();
+      } else {
+        this.location.back();
+      }
     });
   }
   async backButtonAlert() {
     const alert = await this.alertcontroller.create({
-      message: 'you just pressed the back button!'
+      message: 'you just pressed the back button!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Close App',
+          handler: () => {
+            navigator['app'].exitApp();
+        }
+      }]
     });
     await alert.present();
   }
